8061088eafef165314dd61e5fcb8e815586b9d21
Mohit-022 <mohitdocoffice@gmail.com>
2026-08-04 19:21:12 +0530

    make linkedlist folder

---
+++ LinkedList/implementation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node {
+public:
+    int val;
+    node* next;
+    node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+class LL{
+public:         
+    void addHead(int val){
+        
+    }
+};
+int main(){
+
+
+}
