ffc9e2c191a705c6adaa93979835ad340adf5115
Mohit-022 <mohitdocoffice@gmail.com>
2026-08-20 02:07:31 +0530

    enhance readability

--- Stack/LeetCode Quest/Hard/84_Largest_Rectangle_in_Histogram.cpp
+++ Stack/LeetCode Quest/Hard/84_Largest_Rectangle_in_Histogram.cpp
@@ -8,6 +8,7 @@ public:
         vector<int>psei(n,-1);    // previous smaller element index
         stack<int>st;
         int mx=INT_MIN;
+        int area=0;
         for(int i=0;i<n;i++){
             while(!st.empty() && heights[st.top()]>=heights[i]){
                 st.pop();
@@ -32,9 +33,9 @@ public:
             st.push(i);
 
             // yhi pe area nikal kr... max v store kr rha hu mx me... bad me mx return kr dunga
-            // ab nsei == are of rectagle hai jo maximum ho skta hai
-            nsei[i]= (nsei[i]-psei[i]-1)*heights[i]; 
-            mx=max(mx,nsei[i]);
+            
+            area= (nsei[i]-psei[i]-1)*heights[i]; 
+            mx=max(mx,area);
         }
         return mx;
     }
