5cf2a4229055a3925942211e021860c0344e373b
Mohit-022 <mohitdocoffice@gmail.com>
2026-08-25 01:15:43 +0530

    Nth level printing

---
+++ Binary Tree/nth_level_printing.cpp
@@ -0,0 +1,68 @@
+// Pre Order traversal
+
+#include<bits/stdc++.h>
+using namespace std;
+class tree{
+public:
+    int val;
+    tree *left;
+    tree *right;
+
+    tree(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+
+    
+};
+
+void nthLevelPrint(tree *root,int curr, int level){ // curr == current level,  level== which level i want to print
+    if(root ==NULL) return;
+    if(curr==level) {
+        cout<<root->val<<" ";
+        return;
+    }
+    
+    nthLevelPrint(root->left,curr+1,level); // left
+    nthLevelPrint(root->right,curr+1,level); // right
+}
+void nthLevelPrint_reverse(tree *root,int curr, int level){ // curr == current level,  level== which level i want to print
+    if(root ==NULL) return;
+    if(curr==level) {
+        cout<<root->val<<" ";
+        return;
+    }
+    nthLevelPrint_reverse(root->right,curr+1,level); // right
+    nthLevelPrint_reverse(root->left,curr+1,level);  // left
+    
+}
+
+int main(){
+    
+    tree *a=new tree(1);   // root node
+    tree *b=new tree(2);
+    tree *c=new tree(3);
+    tree *d=new tree(4);
+    tree *e=new tree(5);
+    tree *f=new tree(6);
+    tree *g=new tree(7);
+    // tree *h=new tree(8);
+    // tree *i=new tree(9);
+
+    a->left=b;  //root node
+    a->right=c;
+    b->left=d;
+    b->right=e;
+    c->left=f;
+    c->right=g;
+    // g->right=h;
+    // h->right=i;
+
+   nthLevelPrint(a,1,3);
+   cout<<endl;
+   nthLevelPrint_reverse(a,1,3);
+    
+
+
+}
