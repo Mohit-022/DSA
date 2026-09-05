acc9434032a2bd2d86167d39f2badbeaaffd5a1f
Mohit-022 <mohitdocoffice@gmail.com>
2026-09-06 00:04:02 +0530

    LeetCode 701

---
+++ Binary Search Tree/LeetCode question/Medium/701_Insert_into_a_Binary_Search_Tree.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    void insert(TreeNode* root, int val){
+        if(root->val > val){
+            if(root->left == NULL){
+                root->left =new TreeNode(val);
+            }
+            else{
+                insert(root->left,val);
+            }
+        }
+        else // if(root->val < val)
+        {
+            if(root->right == NULL){
+                root->right =new TreeNode(val);
+            }
+            else{
+                insert(root->right,val);
+            }
+        }
+    }
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL) {
+            root=new TreeNode(val);
+            return root;
+        }
+        else{
+            insert(root, val);
+            return root;
+        }
+    }
+};
