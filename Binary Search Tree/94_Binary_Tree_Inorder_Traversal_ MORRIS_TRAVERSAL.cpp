1209cfdd3692332066e1166682be1aba537511ac
Mohit-022 <mohitdocoffice@gmail.com>
2026-09-12 04:08:11 +0530

    Morris traversal

---
+++ Binary Search Tree/94_Binary_Tree_Inorder_Traversal_ MORRIS_TRAVERSAL.cpp
@@ -0,0 +1,34 @@
+/// morris traversal
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        TreeNode* curr=root;
+        TreeNode* pred=root;
+        while(curr!=NULL){
+            if(curr->left!=NULL){
+                // finding predecessor
+                pred=curr->left;
+                while(pred->right!=NULL && pred->right!=curr){
+                    pred=pred->right;
+                }
+                if(pred->right==NULL){
+                    pred->right=curr;
+                    curr=curr->left;
+                }
+                else if(pred->right==curr){
+                    pred->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+                
+            }
+            else{
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+
+        }
+        return ans;
+    }
+};
