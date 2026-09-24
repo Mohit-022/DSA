b9d7a2c9a22f2cb52c64f9bd28b6cefbf4580605
Mohit-022 <mohitdocoffice@gmail.com>
2026-09-24 17:08:22 +0530

    LeetCode 973

---
+++ Priority_queue (Heaps)/LeetCode Quest/Medium/973_K_Closest_Points_to_Origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    typedef pair<long long, vector<int> >piv; // pair of int & vector
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue< piv >pq;
+        for(int i=0; i<points.size();i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            long long dis= x*x + y*y;
+            pq.push({dis*dis,points[i]});
+            if(pq.size()>k) pq.pop();
+        }
+        vector<vector<int>> ans;
+        while(!pq.empty()){
+            piv p=pq.top();
+            ans.push_back(p.second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
